858cff48c875eb0f0fafdaba3ede01cc08d87b52
Gleb Sevruk <gsev@diceus.com>
2018-11-10 23:49:51 +0200

    Initial commit

---
+++ src/sites/sites.page.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="sites-root">
+
+        <H1>Sites2</H1>
+
+
+        <el-input placeholder="Site Url" v-model="newUrl"></el-input>
+
+        <el-button type="primary" @click="addSite()" :disabled="newUrl === ''">Add</el-button>
+        <el-table
+                :data="all"
+                style="width: 100%">
+            <el-table-column
+                    prop="status"
+                    label="Status"
+                    width="180">
+            </el-table-column>
+            <el-table-column
+                    prop="url"
+                    label="Url"
+                    >
+            </el-table-column>
+
+        </el-table>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "sites.page",
+    data() {
+      return {
+        newUrl: '',
+        all: [
+          {
+            url: 'xxxx',
+            status: 200,
+          }]
+      }
+    },
+    methods: {
+      addSite() {
+        this.all.push({url: this.newUrl})
+        this.clear()
+      },
+      clear() {
+        this.newUrl = ''
+      },
+    },
+  }
+</script>
+
+<style scoped>
+
+</style>
